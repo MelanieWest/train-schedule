6d21b2ff33807221b1745200f635ad118eb60a48
Melanie West <dhmofan@gmail.com>
2018-03-15 16:25:25 -0400

    create a file for firebase connection

---
+++ assets/firelink.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCqpV5bxyeWpf9i3okSllk0vU9BspMPlEE",
+    authDomain: "train-scheduler-a4773.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a4773.firebaseio.com",
+    projectId: "train-scheduler-a4773",
+    storageBucket: "",
+    messagingSenderId: "763740079918"
+  };
+  firebase.initializeApp(config);
+  
